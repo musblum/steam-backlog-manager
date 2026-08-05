d6e48d7ba5e66ceda3953a14721281e98d1f0d84
Salem <musblum@gmail.com>
2026-08-04 23:23:49 -0400

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/salem/steambacklogmanager/SteambacklogmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.salem.steambacklogmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteambacklogmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteambacklogmanagerApplication.class, args);
+	}
+
+}
